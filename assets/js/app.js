ea13dafea3a396b7b211b3095232cb8014df616e
Jonathan <jondtran132@gmail.com>
2021-07-17 22:10:06 -0700

    finished scatter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // @TODO: YOUR CODE HERE!
-var svgWidth = 960;
+var svgWidth = 800;
 var svgHeight = 500;
 
 var margin = {
@@ -19,31 +19,55 @@ var svg = d3.select("#scatter")
 
 var chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
 d3.csv("assets/data/state-data.csv").then(function(stateData) {
     console.log(stateData);
 
     stateData.forEach(function(stuff) {
-        stuff.age = +stuff.age;
         stuff.poverty = +stuff.poverty;
+        stuff.healthcare = +stuff.healthcare;
     });
 
     var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, data => data.age)])
+    .domain([8, d3.max(stateData, data => data.poverty)])
     .range([0, chartWidth]);
 
     var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, data => data.poverty)])
+    .domain([0, d3.max(stateData, data => data.healthcare)])
     .range([chartHeight, 0]);
 
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    chartGroup.append("g").selectAll("dot").enter().append("circle")
-    .attr("cx", function(d) {return x(d.age)})
-    .attr("cy", function(d) {return y(d.poverty)})
-    .attr("r", 1)
-    .style("fill", "blue");
+    chartGroup.selectAll("circle").data(stateData).enter().append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 10)
+    .style("fill", "lightblue");
 
-    chartGroup.append("g").classed("axis", true).call(leftAxis);
-    chartGroup.append("g").classed("axis", true).attr("transform", `translate(0, ${chartHeight})`).call(bottomAxis);
+    chartGroup.selectAll("text").data(stateData).enter()
+    .append("text")
+    .text(d => d.abbr)
+    .attr("dx", d => xLinearScale(d.poverty)-7)
+    .attr("dy", d => yLinearScale(d.healthcare)+3)
+    .attr("fill", "white")
+    .style("font-size", 10);
+
+    chartGroup.append("g").call(leftAxis);
+    chartGroup.append("g").attr("transform", `translate(0, ${chartHeight})`).call(bottomAxis);
+
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (chartHeight/2)-70)
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks in Healthcare (%)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${(chartWidth / 2)-20}, ${chartHeight + (margin.top-20)})`)
+    .attr("class", "axisText")
+    .text("Poverty (%)");
+
+
 });
